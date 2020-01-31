a7bccc793418aad4705b41f554efe8ce76d39eed
Mark Paxton <markpaxton3@gmail.com>
2020-01-31 03:48:39 +0100

    Add sample code solution

---
+++ SampleApp/ApplicationStatePlaceholder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SampleApp
+{
+   public class ApplicationStatePlaceholder : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private bool boolValue;
+      public bool BoolValue
+      {
+         get => boolValue; set
+         {
+            boolValue = value;
+            OnPropertyChanged();
+         }
+      }
+
+      ObservableCollection<string> availableItems = new ObservableCollection<string> { "Item1", "Item2", "Item3", "Item4", "Item5" };
+      public ObservableCollection<string> AvailableItems
+      {
+         get => availableItems;
+         set
+         {
+            availableItems = value;
+            OnPropertyChanged();
+         }
+      }
+      private string selectedItem;
+      public string SelectedItem
+      {
+         get => selectedItem;
+         set
+         {
+            selectedItem = value;
+            OnPropertyChanged();
+         }
+      }
+
+      private int max = 5;
+      public int Max
+      {
+         get => max;
+         set
+         {
+            max = value;
+            OnPropertyChanged();
+         }
+      }
+
+
+
+      protected void OnPropertyChanged([CallerMemberName] string name = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+      }
+
+
+   }
+}
---
+++ SampleApp/DemoControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleApp
+{
+   /// <summary>
+   /// Interaction logic for DemoControl.xaml
+   /// </summary>
+   public partial class DemoControl : UserControl
+   {
+      public DemoControl()
+      {
+         InitializeComponent();
+         button1.Click += button1_Click;
+      }
+
+      private void button1_Click(object sender, RoutedEventArgs e)
+      {
+         ControlValue.AvailableItems.Remove(ControlValue.SelectedItem);
+         ControlValue.SelectedItem = null;
+      }
+
+      public static readonly DependencyProperty ControlValueProperty =
+       DependencyProperty.Register("ControlValue", typeof(ApplicationStatePlaceholder), typeof(DemoControl),
+          new FrameworkPropertyMetadata(new ApplicationStatePlaceholder()));
+
+      public ApplicationStatePlaceholder ControlValue
+      {
+         get { return (ApplicationStatePlaceholder)GetValue(ControlValueProperty); }
+         set { SetValue(ControlValueProperty, value); }
+      }
+   }
+}
